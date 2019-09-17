ef8eb871b942cb054f29ec26442e08d9140f2b33
VeeAyeInIn <veeayeinin@gmail.com>
2019-09-17 11:48:46 -0400

    Added Basic OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/PrimaryOpMode.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * @author NOTN Programming Team
+ * <p>
+ * This is the main {@link OpMode} for use with manual controls via gamepads, phones, or some other
+ * device. The Autonomous task will be handled using {@link com.qualcomm.robotcore.eventloop.opmode.LinearOpMode}.
+ */
+public class PrimaryOpMode extends OpMode {
+
+    /**
+     * This method runs once when the <i>init</i> button is activated.
+     *
+     * @see OpMode#init()
+     */
+    @Override
+    public void init() {
+
+    }
+
+    /**
+     * This method loops when the <i>init</i> button is activated.
+     *
+     * @see OpMode#init_loop()
+     */
+    @Override
+    public void init_loop() {
+
+    }
+
+    /**
+     * This method runs once when the <i>start</i> button is activated.
+     *
+     * @see OpMode#start()
+     */
+    @Override
+    public void start() {
+
+    }
+
+    /**
+     * This method loops once the <i>start</i> button is activated.
+     *
+     * @see OpMode#loop()
+     */
+    @Override
+    public void loop() {
+
+    }
+
+    /**
+     * This method runs once when the <i>stop</i> button is activated.
+     *
+     * @see OpMode#stop()
+     */
+    @Override
+    public void stop() {
+
+    }
+}
