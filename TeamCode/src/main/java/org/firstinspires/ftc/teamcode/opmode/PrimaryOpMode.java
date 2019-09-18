736da3da050bf4cc25f490cb83c985f25d6e7db8
VeeAyeInIn <veeayeinin@gmail.com>
2019-09-18 12:44:09 -0400

    Updated OpModes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/PrimaryOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/PrimaryOpMode.java
@@ -2,22 +2,34 @@ package org.firstinspires.ftc.teamcode.opmode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.teamcode.util.Instance;
+
 /**
  * @author NOTN Programming Team
  * <p>
  * This is the main {@link OpMode} for use with manual controls via gamepads, phones, or some other
  * device. The Autonomous task will be handled using {@link com.qualcomm.robotcore.eventloop.opmode.LinearOpMode}.
  */
-public class PrimaryOpMode extends OpMode {
+public final class PrimaryOpMode extends OpMode {
+
+    private static final Instance<PrimaryOpMode> INSTANCE = new Instance<>();
 
     /**
-     * This method runs once when the <i>init</i> button is activated.
-     *
-     * @see OpMode#init()
+     * Default constructor to initiate storage of its instance.
      */
-    @Override
-    public void init() {
+    public PrimaryOpMode() {
+        // Instance is now tied to this. If you were to create a new version of this class, an error
+        // would be thrown as it would attempt to store an instance when it is already assigned.
+        INSTANCE.store(this);
+    }
 
+    /**
+     * Accesses the static referral to the Object
+     *
+     * @return the static instance
+     */
+    public static PrimaryOpMode getInstance() {
+        return INSTANCE.get();
     }
 
     /**
@@ -31,13 +43,13 @@ public class PrimaryOpMode extends OpMode {
     }
 
     /**
-     * This method runs once when the <i>start</i> button is activated.
+     * This method runs once when the <i>init</i> button is activated.
      *
-     * @see OpMode#start()
+     * @see OpMode#init()
      */
     @Override
-    public void start() {
-
+    public void init() {
+        newStatus("Initialized");
     }
 
     /**
@@ -51,12 +63,32 @@ public class PrimaryOpMode extends OpMode {
     }
 
     /**
+     * This method runs once when the <i>start</i> button is activated.
+     *
+     * @see OpMode#start()
+     */
+    @Override
+    public void start() {
+        newStatus("Started");
+    }
+
+    /**
      * This method runs once when the <i>stop</i> button is activated.
      *
      * @see OpMode#stop()
      */
     @Override
     public void stop() {
+        newStatus("Stopped");
+    }
 
+    /**
+     * Updates the {@link org.firstinspires.ftc.robotcore.external.Telemetry} data for the specific
+     * caption 'Status'.
+     *
+     * @param status the new status of the robot
+     */
+    private void newStatus(String status) {
+        telemetry.addData("Status", status);
     }
 }
