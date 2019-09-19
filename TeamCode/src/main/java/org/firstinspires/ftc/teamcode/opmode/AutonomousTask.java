38efd51c203f660b043dcb77e44abf9adeb04c77
VeeAyeInIn <veeayeinin@gmail.com>
2019-09-19 13:04:23 -0400

    Updated Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/AutonomousTask.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/AutonomousTask.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.teamcode.util.Instance;
 
 /**
@@ -12,19 +14,47 @@ import org.firstinspires.ftc.teamcode.util.Instance;
 public final class AutonomousTask extends LinearOpMode {
 
     private static final Instance<AutonomousTask> INSTANCE = new Instance<>();
+
+    // We can load TFOD assets through this
+    private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
+    private static final String TFOD_STONE_ASSET = "Stone";
+    private static final String TFOD_SKYSTONE_ASSET = "Skystone";
+
+    // Vuforia
     private final VuforiaLocalizer.Parameters parameters;
     private final VuforiaLocalizer localizer;
 
+    // Tensor Flow Object Detection
+    private final TFObjectDetector tfod;
+
+    // Keep track of time
+    private final ElapsedTime time = new ElapsedTime();
+
     /**
-     * Default constructor to initiate storage of its instance.
+     * Default constructor to initiate storage of its instance, along with basic initialization
+     * of all variables.
      */
     public AutonomousTask() {
         // Instance is now tied to this. If you were to create a new version of this class, an error
         // would be thrown as it would attempt to store an instance when it is already assigned.
         INSTANCE.store(this);
+
         parameters = new VuforiaLocalizer.Parameters();
-        parameters.vuforiaLicenseKey = " VUFORIA_KEY ";
+        parameters.vuforiaLicenseKey = "AbyhPYH/////AAABmVllNwWWaUnSozzaWrSP7kuAOpem63iiGDaZXW1b7zxbWr5h4qHCM4YqWKiRliNBlApeot38Nz3iQxRPGeIKlPlEtRbZXBY4nstLBf5mPFHwpq6Ajsr/3G60eThr4G+9KolTe30N2MHtfO0G7PkxkzP7wRPf8fji8+CMCvOxE19ZY6YF0L9MJEK+/p6JiXWO7E97kKcGlcfO85ipV5mC5JL9LVYOcVc5KvjkAwQiteEasU3Fv8kW/s4C1f/HPNqvF9I3jgyNz6HdxF/4OCic6nlJITNiTkKMTOeHYp65SXkFUYDsRKeTEvKQtTOe4Qkn6bmY2jhN2/EU1HC1JIOJ3kTVhsGZ8bWEzKA6aJ5CNuUa";
+
         localizer = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Tensor Flow Object Detector :: Initialization of Detector
+        if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
+            int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                    "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+            TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+            tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, localizer);
+            tfod.loadModelFromAsset(TFOD_MODEL_ASSET, TFOD_STONE_ASSET, TFOD_SKYSTONE_ASSET);
+        } else {
+            tfod = null;
+            telemetry.addData("Error@" + hashCode(), "TFOD Detector could not be created.");
+        }
     }
 
     /**
@@ -41,6 +71,11 @@ public final class AutonomousTask extends LinearOpMode {
      */
     @Override
     public void runOpMode() {
+        // Preform startup actions here that should not be done within the constructor
+        // . . .
 
+        // Wait until the START button has been pressed
+        waitForStart();
+        time.reset();
     }
 }
