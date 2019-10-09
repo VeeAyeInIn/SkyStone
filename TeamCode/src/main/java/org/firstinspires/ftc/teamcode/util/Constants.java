58387cf12ade0636c2b7ca6f045c98406af2f269
VeeAyeInIn <veeayeinin@gmail.com>
2019-10-09 16:22:02 -0400

    Vague Update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Constants.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.util;
+
+public class Constants {
+
+    public static double getStoneLength() {
+        return 4;
+    }
+
+    public static double getStoneWidth() {
+        return 8;
+    }
+
+    public static double getStoneHeight() {
+        return 4;
+    }
+}
