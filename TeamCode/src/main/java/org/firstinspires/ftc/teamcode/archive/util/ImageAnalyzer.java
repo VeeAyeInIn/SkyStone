c9305f436c685eba0e3fe55bafc6829722364bc0
cvi7 <cvi7@k12albemarle.org>
2019-10-14 17:15:51 -0400

    Cleaned Up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/archive/util/ImageAnalyzer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/archive/util/ImageAnalyzer.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.util;
+package org.firstinspires.ftc.teamcode.archive.util;
 
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/AutonomousTask.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/AutonomousTask.java
@@ -9,8 +9,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
-import org.firstinspires.ftc.teamcode.util.Direction;
-import org.firstinspires.ftc.teamcode.util.Instance;
+import org.firstinspires.ftc.teamcode.archive.util.Direction;
+import org.firstinspires.ftc.teamcode.archive.util.Instance;
 
 import java.util.ArrayList;
 import java.util.Collections;
