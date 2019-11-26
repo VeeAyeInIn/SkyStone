fa1045c7698a372c1d5c84f580bf4c97fc7dff86
VeeAyeInIn <veeayeinin@gmail.com>
2019-11-26 12:29:38 -0500

    Buffet Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drafts/vann/BuffetOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drafts/vann/BuffetOpMode.java
@@ -1,37 +1,74 @@
 package org.firstinspires.ftc.teamcode.drafts.vann;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "Buffet OpMode", group = "OpMode")
+/**
+ * Our robot has 2 expansion hubs, named 'Auxiliary' and 'Wheels.'
+ * <p>
+ * The Auxiliary hub consists of...
+ * - the gears,  [DcMotor]
+ * - the arm,    [DcMotor]
+ * - the wrist,  [CRServo]
+ * - the latch.  [Servo]
+ * <p>
+ * The Wheels hub consists of...
+ * - the wheels, [DcMotor]
+ * - the tray.   [CRServo]
+ * <p>
+ * Driving will mainly be dealt with using {@link #gamepad1}, while other movements for doing things
+ * will be dealt with using {@link #gamepad2}, so the prior can concentrate on driving.
+ * <p>
+ * Physical attributes will be dealt with using {@link #hardwareMap}.
+ */
 public class BuffetOpMode extends OpMode {
 
+    // Constants
+    private static final double DEAD_ZONE = 0.1;
+
+    // Dynamic Values
+    private boolean synchronizedGears;
+    private double speed;
+
+    // Motors
     private DcMotor leftFront;
     private DcMotor rightFront;
     private DcMotor leftRear;
     private DcMotor rightRear;
-    private DcMotor arm;
     private DcMotor leftGear;
     private DcMotor rightGear;
+    private DcMotor arm;
 
-    private boolean independentGears = true;
-    private double speed = 1.0;
+    // Continuous Rotational Servos
+    private CRServo wrist;
+    private CRServo tray;
+
+    // Servos
+    private Servo latch;
 
     @Override
     public void init() {
 
+        // Assign all DcMotors
         leftFront = hardwareMap.dcMotor.get("leftFront");
         rightFront = hardwareMap.dcMotor.get("rightFront");
         leftRear = hardwareMap.dcMotor.get("leftRear");
         rightRear = hardwareMap.dcMotor.get("rightRear");
-        arm = hardwareMap.dcMotor.get("arm");
         leftGear = hardwareMap.dcMotor.get("leftGear");
         rightGear = hardwareMap.dcMotor.get("rightGear");
+        arm = hardwareMap.dcMotor.get("arm");
 
-        // Start running the encoders
-        setUniversalMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        // info("Robot has initialized");
+        // Assign all CRServos
+        wrist = hardwareMap.crservo.get("wrist");
+        tray = hardwareMap.crservo.get("tray");
+
+        // Assign all Servos
+        latch = hardwareMap.servo.get("latch");
+
+        // Assign other values
+        synchronizedGears = true;
     }
 
     @Override
@@ -42,178 +79,106 @@ public class BuffetOpMode extends OpMode {
     @Override
     public void start() {
 
-        // Start the time back at zero
-        // getElapsedTime().reset();
-        // info("Robot has started");
+        // Start up all the motors
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftGear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightGear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // This needs to be running at start
+        tray.setPower(1);
     }
 
     @Override
     public void loop() {
 
+        // Toggle speed
         if (gamepad1.a) {
-            // Fast Speed
-            speed = 1.0;
+            speed = 1.00;
         } else if (gamepad1.x) {
-            // Normal Speed
-            speed = 1.5;
+            speed = 0.75;
         } else if (gamepad1.y) {
-            // Slow Speed
-            speed = 2.0;
+            speed = 0.50;
         } else if (gamepad1.b) {
-            // Super Slow Speed
-            speed = 3.0;
+            speed = 0.25;
         }
 
-        // Power in the x-direction
-        double x = Math.abs(gamepad1.left_stick_x) > 0.2 ? gamepad1.left_stick_x : 0;
-
-        // Power in the y-direction
-        double y = Math.abs(gamepad1.left_stick_y) > 0.2 ? gamepad1.left_stick_y : 0;
-
-        // Rotational power
-        double rotate = gamepad1.right_trigger > gamepad1.left_trigger
-                ? gamepad1.right_trigger : -gamepad1.left_trigger;
-
-        // Set each wheel power
-        setWheelPower((y - x - rotate) / speed,
-                (-y - x - rotate) / speed,
-                (y + x - rotate) / speed,
-                (-y + x - rotate) / speed);
-
+        // Toggle gear synchronization
         if (gamepad2.left_bumper) {
-            // No independent gears
-            independentGears = false;
+            synchronizedGears = true;
         } else if (gamepad2.right_bumper) {
-            // Independent gears
-            independentGears = true;
+            synchronizedGears = false;
         }
 
-        // Now we need to deal with the second controller
-        if (gamepad2.right_trigger > 0.1 || gamepad2.left_trigger > 0.1) {
-            setArmPower(gamepad2.right_trigger > gamepad2.left_trigger
-                    ? gamepad2.right_trigger / 2.0 : -gamepad2.left_trigger / 2.0);
-        }
+        // Movement: controlled using the left and right joystick of gamepad1
+        double y = clip(gamepad1.left_stick_y);  // linear power
+        double x = clip(gamepad1.left_stick_x);  // strafe power
+        double r = clip(gamepad1.right_stick_x); // rotational power
+        setWheelPowers(y - x - r, -y - x - r, y + x - r, -y + x - r);
 
-        // Are we doing independent or synchronized gears
-        if (independentGears) {
-
-            // Handle the gears if not in the dead-zone
-            setGearPower(Math.abs(gamepad2.left_stick_y) > 0.1 ? gamepad2.left_stick_y : 0,
-                    Math.abs(gamepad2.right_stick_y) > 0.1 ? gamepad2.right_stick_y : 0);
+        // Gears
+        double max; // Hold a slot if we need to find the max value
+        if (synchronizedGears) { // Synchronized or independent movements
+            if ((max = Math.max(gamepad2.left_trigger, gamepad2.right_trigger)) > DEAD_ZONE) {
+                leftGear.setPower(max);
+                rightGear.setPower(max);
+            } else {
+                leftGear.setPower(0);
+                rightGear.setPower(0);
+            }
         } else {
+            leftGear.setPower(clip(gamepad2.left_stick_y));
+            rightGear.setPower(-clip(gamepad2.right_stick_y));
+        }
 
-            // Handle the gears if not in the dead-zone
-            setGearPower(Math.abs(gamepad2.left_stick_y) > Math.abs(gamepad2.right_stick_y)
-                    ? Math.abs(gamepad2.left_stick_y) > 0.1
-                    ? gamepad2.left_stick_y
-                    : 0
-                    : Math.abs(gamepad2.right_stick_y) > 0.1
-                    ? gamepad2.right_stick_y
-                    : 0);
+        // Arm
+        arm.setPower(clip(gamepad2.left_stick_y));
+
+        // Wrist
+        wrist.setPower(clip(gamepad2.right_stick_x));
+
+        // Latch
+        if (gamepad2.dpad_up) {
+            latch.setPosition(latch.getPosition() + 0.01);
+        } else if (gamepad2.dpad_down) {
+            latch.setPosition(latch.getPosition() - 0.01);
         }
     }
 
     @Override
     public void stop() {
 
-        // Stop the encoders
-        setUniversalMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        // info("Robot has stopped");
+        // Stop (and reset) all the motors
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftGear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightGear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
-    /**
-     * Sets the universal power for each wheel.
-     *
-     * @param power the power of each wheels
-     */
-    public void setWheelPower(final double power) {
-        setWheelPower(power, power, power, power);
+    private void setWheelPowers(double lf, double rf, double lr, double rr) {
+        leftFront.setPower(lf * speed);
+        rightFront.setPower(rf * speed);
+        leftRear.setPower(lr * speed);
+        rightRear.setPower(rr * speed);
     }
 
-    /**
-     * Sets the power for each individual wheel.
-     *
-     * @param leftFrontPower  the power of the left front wheel
-     * @param rightFrontPower the power of the right front wheel
-     * @param leftRearPower   the power of the left rear wheel
-     * @param rightRearPower  the power of the right rear wheel
-     */
-    public void setWheelPower(final double leftFrontPower, final double rightFrontPower,
-                              final double leftRearPower, final double rightRearPower) {
-        leftFront.setPower(leftFrontPower);
-        rightFront.setPower(rightFrontPower);
-        leftRear.setPower(leftRearPower);
-        rightRear.setPower(rightRearPower);
+    private double clip(double check, double min, double max) {
+        if (Math.abs(check) < min) {
+            return 0;
+        } else if (Math.abs(check) > max) {
+            return max;
+        } else {
+            return check;
+        }
     }
 
-    /**
-     * Sets the universal power for each gear.
-     *
-     * @param power the power of each gear
-     */
-    public void setGearPower(final double power) {
-        setGearPower(power, power);
-    }
-
-    /**
-     * Sets the power for each individual gear.
-     *
-     * @param leftGearPower  the power of the left gear
-     * @param rightGearPower the power of the right gear
-     */
-    public void setGearPower(final double leftGearPower, final double rightGearPower) {
-        leftGear.setPower(leftGearPower);
-        rightGear.setPower(rightGearPower);
-    }
-
-    /**
-     * Sets the power of the arm.
-     *
-     * @param power the power of the arm
-     */
-    public void setArmPower(final double power) {
-        arm.setPower(power);
-    }
-
-    /**
-     * Sets the mode for each of the wheels.
-     *
-     * @param mode the wheel motor mode
-     */
-    public void setWheelMode(DcMotor.RunMode mode) {
-        leftFront.setMode(mode);
-        rightFront.setMode(mode);
-        leftRear.setMode(mode);
-        rightRear.setMode(mode);
-    }
-
-    /**
-     * Sets the mode for each of the gears.
-     *
-     * @param mode the gear motor mode
-     */
-    public void setGearMode(DcMotor.RunMode mode) {
-        leftGear.setMode(mode);
-        rightGear.setMode(mode);
-    }
-
-    /**
-     * Sets the mode for the arm.
-     *
-     * @param mode the arm motor mode
-     */
-    public void setArmMode(DcMotor.RunMode mode) {
-        arm.setMode(mode);
-    }
-
-    /**
-     * Sets the universal mode for all of the motors.
-     *
-     * @param mode the universal motor mode
-     */
-    public void setUniversalMode(DcMotor.RunMode mode) {
-        setWheelMode(mode);
-        setGearMode(mode);
-        setArmMode(mode);
+    private double clip(double check) {
+        return clip(check, DEAD_ZONE, 1.0);
     }
 }
