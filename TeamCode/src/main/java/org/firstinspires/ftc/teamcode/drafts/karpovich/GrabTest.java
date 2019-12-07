03b7d2da49336dc7c7756d37ac14eeb506f9eaa5
Daniel Karpovich <piratedk727@gmail.com>
2019-12-06 19:48:43 -0500

    I did a thing v8.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drafts/karpovich/GrabTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drafts/karpovich/GrabTest.java
@@ -12,6 +12,7 @@ import java.util.Locale;
 @TeleOp(name = "GrabTest", group = "Iterative OpMode")
 public class GrabTest extends OpMode {
     private Servo grabber;
+    private CRServo gate;
 
     @Override
     public void init() {
@@ -25,12 +26,18 @@ public class GrabTest extends OpMode {
         telemetry.update();
 
         grabber = hardwareMap.servo.get("grabber");
+        gate = hardwareMap.crservo.get("gate");
     }
 
     public void loop()
     {
         if (gamepad2.dpad_up) {
             grabber.setPosition(90.0);
+        } else if (gamepad2.dpad_down){
+            grabber.setPosition(0.0);
+        }
+        if (Math.abs(gamepad2.left_stick_y) > .1){
+            gate.setPower(gamepad2.left_stick_y);
         }
     }
 }
