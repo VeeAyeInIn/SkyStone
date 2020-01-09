52c0ae327fefcaca709b8e128065801cbbaf19b5
piratedk727 <piratedk727@gmail.com>
2020-01-09 17:16:22 -0500

    IDK

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drafts/karpovich/SkystoneAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drafts/karpovich/SkystoneAuto.java
@@ -72,7 +72,7 @@ public class SkystoneAuto extends LinearOpMode {
 
         //Test movement
         this.move(10.0);
-        this.pause(1.0);
+        this.pause(2.0);
         this.back(10.0);
 
         //Actual Movement
@@ -131,9 +131,9 @@ public class SkystoneAuto extends LinearOpMode {
         rightRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         leftFront.setPower(0.5);
-        rightFront.setPower(-0.5);
+        rightFront.setPower(0.5);
         leftRear.setPower(0.5);
-        rightRear.setPower(-0.5);
+        rightRear.setPower(0.5);
 
         while (leftFront.isBusy() && rightFront.isBusy() && leftRear.isBusy() && rightRear.isBusy()) {
             telemetry.addData("MOTOR LF", String.format(Locale.ENGLISH, "POS: %s, POW: %s", leftFront.getTargetPosition(), leftFront.getPower()));
